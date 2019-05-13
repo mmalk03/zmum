92fe30225d3ce2f61dbf9a0b7a0286f0bc0b8406
276953 malkinski mikolaj <m.malkinski87@gmail.com>
2019-05-13 10:03:10 +0200

    Lab 11 - Feature selection task 3 and 4

--- lab/src/lab11-feature-selection.R
+++ lab/src/lab11-feature-selection.R
@@ -74,3 +74,56 @@ for (sigma in sigmaSeq) {
 plot(sigmaSeq, corr, col = 'red', type = 'l', ylim = c(0, max(c(corr, mi))))
 lines(sigmaSeq, mi, col = 'blue')
 
+# Task 3
+library('glmnet')
+
+n <- 100
+p <- 1000
+true <- c(1, 2, 3)
+betas <- c(1, 1, 1, rep(0, p - 3))
+x <- matrix(rnorm(n * p), ncol = p)
+p <- 1 / (1 + exp(-x %*% betas))
+y <- rbinom(n, 1, p)
+
+cv.glmnet1 <- cv.glmnet(x, y, family = 'binomial')
+lambda_opt <- cv.glmnet1$lambda.1se
+model <- glmnet(x, y, family = 'binomial', lambda = lambda_opt)
+coefficients <- model$beta
+
+est_true1 <- which(coefficients != 0)
+recall1 <- length(intersect(true, est_true1)) / length(true)
+prec1 <- length(intersect(true, est_true1)) / length(est_true1)
+
+d <- data.frame(x, y)
+biggest <- formula(glm(y ~ ., data = d))
+min.model <- glm(y ~ 1, data = d)
+m2 <- step(min.model, data = d, direction = 'forward', scope = biggest, k = log(n))
+
+library('hdi')
+m3 <- multi.split(x, y)
+
+# Task 4
+n <- 200
+p <- 20
+B <- 100
+coefs <- list()
+j <- 0
+for (p in c(5, 10, 20, 40, 50)) {
+  coef1 <- numeric(B)
+  for (k in 1:B) {
+    true <- c(1, 2, 3)
+    x <- matrix(rnorm(n * p), nrow = n, ncol = p)
+    b <- numeric(p)
+    b[true] <- 1
+    eta <- x %*% b
+    probs <- 1 / (1 + (exp(-eta)))
+    y <- rbinom(n, 1, probs)
+    d <- data.frame(x, y)
+    m <- glm(y ~ ., data = d)
+    coef1[k] <- m$coef[2]
+  }
+  j <- j + 1
+  coefs[[j]] <- coef1
+}
+sapply(coefs, var)
+boxplot(coefs)
